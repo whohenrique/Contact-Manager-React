95fb4970dcb580d9c51a02bd894619f7ffa1ac70
Henrique <heduardocr7@gmail.com>
2024-03-01 19:55:07 -0300

    Creating Components, Pages and Styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import './App.css'
+import Home from "./pages/Home"
 
 export default function App() {
-
-
   return (
-    <>
-
-    </>
+    <div className="App">
+      <Home />
+    </div>
   )
 }
---
+++ src/types/contact.d.ts
@@ -0,0 +1,6 @@
+export default interface ContactProps {
+    id: any | null;
+    name: string;
+    email: string;
+    phone: string;
+}
